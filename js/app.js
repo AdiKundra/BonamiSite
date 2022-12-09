1d359206e8dda8775e41a1283ca7674e8a793208
= <=>
2022-12-09 14:14:31 +0530

    home page change's

--- js/app.js
+++ js/app.js
@@ -76,3 +76,16 @@ lists.forEach(el => {
     })
 });
 
+
+
+// accordion
+
+$(function () {
+  $(".accordion-content:not(:first-of-type)").css("display", "none");
+  $(".js-accordion-title:first-of-type").addClass("open");
+ 
+  $(".js-accordion-title").click(function () {
+    $(".open").not(this).removeClass("open").next().slideUp(300);
+    $(this).toggleClass("open").next().slideToggle(300);
+  });
+});

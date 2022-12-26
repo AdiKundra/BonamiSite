5fc76687ec667855058d08e9c8c2110209f37846
= <=>
2022-12-26 12:11:54 +0530

    home page done

--- js/app.js
+++ js/app.js
@@ -20,9 +20,9 @@ $('.top-icon-slider').slick({
       }
     },
     {
-      breakpoint: 1024,
+      breakpoint: 1025,
       settings: {
-        slidesToShow: 3,
+        slidesToShow: 4,
         slidesToScroll: 1,
         infinite: false,
         dots: false
